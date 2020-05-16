eeaf7387c2b56a8b5b2c728c2c0b2f24593e3066
皮皮虾 <2641543334@qq.com>
2020-05-16 11:41:16 +0800

    使用第2种方式实现登陆功能

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,39 +1,36 @@
 // 1. 写入口函数
 $(function () {
-  // 2. 给登陆按钮注册事件
-  $('.input_sub').on('click', function (e) {
-    // 阻止默认提交 行为
+  // 2. 给form表单注册submit事件
+  $('.login_form').on('submit',function(e){
+    // 3. 阻止默认提交行为
     e.preventDefault()
-    // 3. 获取登陆的数据  username password
-    var username = $('.input_txt').val()
-    var password = $('.input_pass').val()
-    // 4. 判断用户名和密码是否为空
-    if($.trim(username)==''||$.trim(password)==''){
-      // 5. 如果数据没有问题则发送请求，反之不能发送请求
-      alert('用户名或密码不能为空,请重新输入...')
-      return; // 阻止代码的向下执行
-    }
 
-    // 6. 发送ajax请求
+    // 4. 发送ajax请求
     $.ajax({
       type:'post',
       url:'http://localhost:8080/api/v1/admin/user/login',
-      data:{
-        username:username,
-        password:password
+      data:$(this).serialize(),
+      beforeSend:function(){
+        // 5. 发送请求之前验证用户名或密码是否为空
+        var flag = false 
+        $('.login_form input[name]').each(function(index,ele){
+          if($.trim($(ele).val())==''){
+            flag = true
+          }
+        })
+        if(flag){
+          alert('输入的用户名或密码不能为空')
+          return false;// 阻止请求的发送
+        }
       },
       success:function(res){
-        // console.log(res);
-        // console.log(typeof res);
         if(res.code==200){
-          alert('登陆成功...')
-          // 7. 跳转到主页面
+          alert('登陆成功')
           window.location.href = './index.html'
         }else {
-          alert(res.msg) // 提示错误信息
+          alert(res.msg)
         }
       }
     })
   })
-
 })
