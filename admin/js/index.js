7fb6009177c3d2104c8d60286968a83d08b44df8
elongpaox <46686856@qq.com>
2020-05-16 20:36:16 +0800

    实现登出按钮并与名字于头像实时同步

---
+++ admin/js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+    // 一打开网站就返送ajax请求，获取网站管理员用户的个人信息
+    $.ajax({
+        // 用户信息
+        url: window.BigNew.user_info,
+        // 设置请求头，把刚刚开始储存到浏览器内存中的token令牌带过去
+        headers: {
+            Authorization: window.localStorage.getItem('token')
+        },
+        success: function (res) {
+            console.log(res);
+            if (res.code == 200) {
+                $(".user_info>span>i").text(res.data.nickname);
+                $(".user_info>img").attr("src", res.data.userPic);
+                $(".user_center_link>img").attr("src", res.data.userPic);
+            }
+        }
+    })
+    // 登出
+    $(".logout").on("click", function () {
+        // 删除token，跳转到登入页面
+        window.localStorage.removeItem("token");
+        window.location.href = "./login.html"
+    })
+})
