a79fb5f7d4fdd02be4b785e3d52207f4d394ea7e
elongpaox <46686856@qq.com>
2020-05-17 19:14:47 +0800

    列表栏完成

--- admin/js/index.js
+++ admin/js/index.js
@@ -5,7 +5,6 @@ $(function () {
         url: window.BigNew.user_info,
         // 设置请求头，把刚刚开始储存到浏览器内存中的token令牌带过去
         headers: {
-            // 
             Authorization: window.localStorage.getItem('token')
         },
         success: function (res) {
@@ -25,5 +24,23 @@ $(function () {
         // 删除token，跳转到登入页面
         window.localStorage.removeItem("token");
         window.location.href = "./login.html"
+    })
+    // 点击高亮
+    $("div.level01 ").on("click", function () {
+        console.log("123");
+        $(this).addClass("active").siblings("div").removeClass("active");
+        if ($(this).index() == 1) {
+            // 显示或者隐藏这个下拉框
+            $(".level02").slideToggle();
+            // 设置小箭头的旋转
+            $(this).find("b").toggleClass("rotate0");
+            // 打开时下拉框的时候默认选中第一个
+            $(".level02 li:eq(0) a").get(0).click();
+            // $("ul.level02>li:eq(0)>a")[0].click();
+        }
+    })
+    // 点击下拉的时候高亮
+    $(".level02 li").on("click", function () {
+        $(this).addClass("active").siblings("li").removeClass("active");
     })
 })
