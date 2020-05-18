384a048506e4ef7d1fb8df2085d00463841d495d
皮皮虾 <2641543334@qq.com>
2020-05-18 15:15:14 +0800

    文章页面的列表渲染完成

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -9,4 +9,25 @@ $(function(){
       $('#selCategory').html(htmlStr)
     }
   })
+
+
+  // 2. 显示当前页面中的所有的文章数据
+  $.ajax({
+    type:'get',
+    url:BigNew.article_query,
+    data:{
+      key:$('#key').val(),
+      type:$('#selCategory').val(),
+      state:$('#selStatus').val(),
+      page:1,
+      perpage:7
+    },
+    success:function(res){
+      console.log(res);
+      if(res.code==200){
+        var htmlStr = template('articleList',res.data)
+        $('tbody').html(htmlStr)
+      }
+    }
+  })
 })
