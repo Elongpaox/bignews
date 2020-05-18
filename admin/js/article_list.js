44390dc86e09cfeab9239e3740e40c216707c070
皮皮虾 <2641543334@qq.com>
2020-05-18 15:36:56 +0800

    文章列表页引入分页插件

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function () {
   // 1. 发送ajax请求获取文章所有的分类 
   $.ajax({
-    type:'get',
-    url:BigNew.category_list,
-    success:function(res){
+    type: 'get',
+    url: BigNew.category_list,
+    success: function (res) {
       // console.log(res);
-      var htmlStr = template('categoryList',res)
+      var htmlStr = template('categoryList', res)
       $('#selCategory').html(htmlStr)
     }
   })
@@ -13,22 +13,22 @@ $(function(){
 
   // 2. 显示当前页面中的所有的文章数据
   getDataByParams({
-    key:$('#key').val(),
-    type:$('#selCategory').val(),
-    state:$('#selStatus').val(),
-    page:1,
-    perpage:7
+    key: $('#key').val(),
+    type: $('#selCategory').val(),
+    state: $('#selStatus').val(),
+    page: 1,
+    perpage: 7
   })
 
-  function getDataByParams(obj){
+  function getDataByParams(obj) {
     $.ajax({
-      type:'get',
-      url:BigNew.article_query,
-      data:obj,
-      success:function(res){
-        console.log(res);
-        if(res.code==200){
-          var htmlStr = template('articleList',res.data)
+      type: 'get',
+      url: BigNew.article_query,
+      data: obj,
+      success: function (res) {
+        console.log(res)
+        if (res.code == 200) {
+          var htmlStr = template('articleList', res.data)
           $('tbody').html(htmlStr)
         }
       }
@@ -37,17 +37,26 @@ $(function(){
 
   // 3. 给筛选按钮注册事件， 根据条件来查询文章数据渲染出来
   // 3.1 给筛选按钮注册事件
-  $('#btnSearch').on('click',function(e){
+  $('#btnSearch').on('click', function (e) {
     // 3.2 阻止默认行为
     e.preventDefault()
     // alert(123)
     // 3.3 调用函数来获取数据
     getDataByParams({
-      key:$('#key').val(),
-      type:$('#selCategory').val(),
-      state:$('#selStatus').val(),
-      page:1,
-      perpage:10
+      key: $('#key').val(),
+      type: $('#selCategory').val(),
+      state: $('#selStatus').val(),
+      page: 1,
+      perpage: 10
     })
+  })
+
+  // 4. 实现分页功能
+  $('#pagination-demo').twbsPagination({
+    totalPages: 35,
+    visiblePages: 7,
+    onPageClick: function (event, page) {
+      $('#page-content').text('Page ' + page)
+    }
   })
 })
