1d7ec5a1b75a3f3d3dedd8d27ae535a2a8897036
皮皮虾 <2641543334@qq.com>
2020-05-18 09:42:42 +0800

    文章分类之单击编辑按钮数据回显完成

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -1,15 +1,15 @@
-$(function(){
+$(function () {
   // 1. 发送请求获取数据，渲染页面
   // 1.1 发送ajax请求
   $.ajax({
-    type:'get',
-    url:BigNew.category_list,
-    success:function(res){
-      console.log(res);
-      console.log(typeof res);
+    type: 'get',
+    url: BigNew.category_list,
+    success: function (res) {
+      console.log(res)
+      console.log(typeof res)
       // 1.2 获取数据并渲染页面
-      if(res.code==200){
-        var htmlStr = template('categoryList',res)
+      if (res.code == 200) {
+        var htmlStr = template('categoryList', res)
         $('tbody').html(htmlStr)
       }
     }
@@ -17,7 +17,7 @@ $(function(){
 
   // 2. 模态框的展示
   // 2.1 给新增按钮注册事件 弹出模态框 
-  $('#xinzengfenlei').on('click',function(){
+  $('#xinzengfenlei').on('click', function () {
     // 2.2 显示模态框
     $('.addModal').modal('show')
 
@@ -30,11 +30,28 @@ $(function(){
   // 3. 编辑按钮
   // 3.1 给编辑按钮注册事件 弹出模态框 要使用委托的方式业注册事件
   // 因为当前的编辑按钮是通过模板创建出来的，相当于是动态创建出来的元素，直接注册事件，是不起作用的，所以需要使用委托的方式来注册事件
-  $('tbody').on('click','.btn-edit',function(){
+  $('tbody').on('click', '.btn-edit', function () {
     // 3.2 显示模态框
     $('.addModal').modal('show')
-     // 2.3 修改提示标题
-     $('.addModal h4').text('更新文章分类')
+    // 2.3 修改提示标题
+    $('.addModal h4').text('更新文章分类')
+
+    // 2.4 发送ajax请求 
+    $.ajax({
+      type: 'get',
+      url: BigNew.category_search,
+      data: {
+        id: $(this).data('id')
+      },
+      success: function (res) {
+        // console.log(res)
+        if(res.code==200){
+          $('#myForm input[name=id]').val(res.data[0].id)
+          $('#myForm input[name=name]').val(res.data[0].name)
+          $('#myForm input[name=slug]').val(res.data[0].slug)
+        }
+      }
+    })
   })
 
   // CRUD
