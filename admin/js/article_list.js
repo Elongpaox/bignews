4485137b05c826167e71ab4000a077152a5f53d8
皮皮虾 <2641543334@qq.com>
2020-05-18 15:33:37 +0800

    文章列表页面的筛选功能实现

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -12,22 +12,42 @@ $(function(){
 
 
   // 2. 显示当前页面中的所有的文章数据
-  $.ajax({
-    type:'get',
-    url:BigNew.article_query,
-    data:{
+  getDataByParams({
+    key:$('#key').val(),
+    type:$('#selCategory').val(),
+    state:$('#selStatus').val(),
+    page:1,
+    perpage:7
+  })
+
+  function getDataByParams(obj){
+    $.ajax({
+      type:'get',
+      url:BigNew.article_query,
+      data:obj,
+      success:function(res){
+        console.log(res);
+        if(res.code==200){
+          var htmlStr = template('articleList',res.data)
+          $('tbody').html(htmlStr)
+        }
+      }
+    })
+  }
+
+  // 3. 给筛选按钮注册事件， 根据条件来查询文章数据渲染出来
+  // 3.1 给筛选按钮注册事件
+  $('#btnSearch').on('click',function(e){
+    // 3.2 阻止默认行为
+    e.preventDefault()
+    // alert(123)
+    // 3.3 调用函数来获取数据
+    getDataByParams({
       key:$('#key').val(),
       type:$('#selCategory').val(),
       state:$('#selStatus').val(),
       page:1,
-      perpage:7
-    },
-    success:function(res){
-      console.log(res);
-      if(res.code==200){
-        var htmlStr = template('articleList',res.data)
-        $('tbody').html(htmlStr)
-      }
-    }
+      perpage:10
+    })
   })
 })
