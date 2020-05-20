f568faa03a4b2275c6a5be90058ec74b16ce9ee8
皮皮虾 <2641543334@qq.com>
2020-05-20 10:23:01 +0800

    文章列表页之使用原始的方式重新实现分页及分页控件重绘

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -10,42 +10,77 @@ $(function () {
     }
   })
 
+  // 2. 显示文章数据
+  // 2.1 一跳转到当前这个页面就要发送ajax请求
+  $.ajax({
+    type: 'get',
+    url: BigNew.article_query,
+    data: {
+      key: $('#key').val(),
+      type: $('#selCategory').val(),
+      state: $('#selStatus').val(),
+      page: 1,
+      perpage: 7
+    },
+    success: function (res) {
+      console.log(res)
+      if (res.code == 200) {
+        // 2.2 渲染数据
+        var htmlStr = template('articleList', res.data)
+        $('tbody').html(htmlStr)
 
-  // 2. 显示当前页面中的所有的文章数据
-  getDataByParams({
-    key: $('#key').val(),
-    type: $('#selCategory').val(),
-    state: $('#selStatus').val(),
-    page: 1,
-    perpage: 7
+        // 2.3 实现分页功能
+        pagination(res)
+      }
+    }
   })
 
-  // 封装了一个根据不同参数获取数据的函数
-  function getDataByParams(obj) {
-    $.ajax({
-      type: 'get',
-      url: BigNew.article_query,
-      data: obj,
-      success: function (res) {
-        console.log(res)
-        if (res.code == 200) {
-          var htmlStr = template('articleList', res.data)
-          $('tbody').html(htmlStr)
+  // 3. 分页功能的函数
+  function pagination(res, visiblePages) {
+    $('#pagination-demo').twbsPagination({
+      totalPages: res.data.totalPage, // 总页数
+      visiblePages: visiblePages || 7, // 可见最大上限页码值
+      first: '首页',
+      last: '最后一页',
+      next: '下一页',
+      prev: '上一页',
+      initiateStartPageClick: false, // 不要默认点击 
+      onPageClick: function (event, page) {
+        //  console.log(event,page);
+        // page是当前页码
+        $.ajax({
+          type: 'get',
+          url: BigNew.article_query,
+          data: {
+            key: $('#key').val(),
+            type: $('#selCategory').val(),
+            state: $('#selStatus').val(),
+            page: page,
+            perpage: 7
+          },
+          success: function (res) {
+            console.log(res)
+            if (res.code == 200) {
+              // 2.2 渲染数据
+              var htmlStr = template('articleList', res.data)
+              $('tbody').html(htmlStr)
 
-          // 服务器端的数据响应回来了之后, 启用分页功能
-          pagination(res.data.totalPage)
-        }
+              // 2.3 实现分页功能
+              pagination(res)
+            }
+          }
+        })
       }
     })
   }
 
-  // 3. 给筛选按钮注册事件， 根据条件来查询文章数据渲染出来
-  // 3.1 给筛选按钮注册事件
-  $('#btnSearch').on('click', function (e) {
-    // 3.2 阻止默认行为
+  // 4. 给筛选按钮注册事件 根据新条件渲染页面
+  // 4.1 给筛选按钮注册事件
+  $('#btnSearch').on('click',function(e){
+    // 4.2 阻止默认的请求行为
     e.preventDefault()
-    // alert(123)
-    // 3.3 调用函数来获取数据
+
+    // 4.3 发送请求获取数据
     $.ajax({
       type: 'get',
       url: BigNew.article_query,
@@ -59,45 +94,16 @@ $(function () {
       success: function (res) {
         console.log(res)
         if (res.code == 200) {
+          // 4.4  渲染数据
           var htmlStr = template('articleList', res.data)
           $('tbody').html(htmlStr)
 
-          // 服务器端的数据响应回来了之后, 更新分页控件 也就是更新总页码
-          // 要使用一个事件changeTotalPages，动态的改变总页码  内部底层就会重绘分页控件
-          // 第1个参数 是事件名称
-          // 第2个参数 是总页码
-          // 第3个参数 是默认的当前页
-          $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, 1)
+          // 4.5 更新分页控件的总页码
+          $('#pagination-demo').twbsPagination('changeTotalPages',res.data.totalPage,1)
         }
       }
     })
-
-
   })
-
-  // 4. 实现分页功能
-  function pagination(totalPages, visiblePages) {
-    $('#pagination-demo').twbsPagination({
-      totalPages: totalPages, // 总页数
-      visiblePages: visiblePages || 7, // 可见最大上限页码值
-      first: '首页',
-      last: '最后一页',
-      next: '下一页',
-      prev: '上一页',
-      initiateStartPageClick: false, // 不要默认点击 
-      onPageClick: function (event, page) {
-        //  console.log(event,page);
-        // page是当前页码
-        getDataByParams({
-          key: $('#key').val(),
-          type: $('#selCategory').val(),
-          state: $('#selStatus').val(),
-          page: page,
-          perpage: 7
-        })
-      }
-    })
-  }
 })
 
 /**
