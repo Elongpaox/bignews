edfbf34701349d9c3d9e23c2292065cf4882e948
皮皮虾 <2641543334@qq.com>
2020-05-20 15:47:57 +0800

    项目中新一个工具函数utils.js

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -1,3 +1,7 @@
 $(function(){
-  console.log(window.location.search); // location.search获取URL地址栏中的?后面的参数数据
+  // console.log(window.location.search); // location.search获取URL地址栏中的?后面的参数数据
+  // "?id=20"
+  var str = location.search.slice(1)
+  var id = utils.convertToObj(str).id
+  console.log(id);
 })
