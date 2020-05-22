8576a5d5d7ff975d49cb95ddb3f3f939d9b1fc5b
皮皮虾 <2641543334@qq.com>
2020-05-22 10:35:26 +0800

    前台index页面的最新资讯一周热门排行最新评论完成

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(function(){
     type:'get',
     url:BigNew.category_list,
     success:function(res){
-      console.log(res);
+      // console.log(res);
       // 1.2 使用模板引擎渲染页面
       if(res.code==200){
         // 原始的做法是使用两个模板
@@ -19,6 +19,58 @@ $(function(){
         var htmlStr = template('categoryList',res)
         $('.menu .level_two').html('<li class="up"></li>'+htmlStr)
         $('.menu .left_menu').html(htmlStr)
+      }
+    }
+  })
+
+  // 2. 热点图数据显示
+  $.ajax({
+    type:'get',
+    url:BigNew.hotPic_news,
+    success:function(res){
+      // console.log(res);
+      if(res.code==200){
+        var htmlStr = template('hotPicTmp',res)
+        $('.main_con .focus_list').html(htmlStr)
+      }
+    }
+  })
+
+  // 3. 最新资讯
+  $.ajax({
+    type:'get',
+    url:BigNew.latest_news,
+    success:function(res){
+      // console.log(res);
+      if(res.code==200){
+        var htmlStr = template('latestNewsTmp',res)
+        $('.common_news').html(htmlStr)
+      }
+    }
+  })
+
+  // 4. 一周热门排行
+  $.ajax({
+    type:'get',
+    url:BigNew.hotrank_list,
+    success:function(res){
+      // console.log(res);
+      if(res.code==200){
+        var htmlStr = template('rankList',res)
+        $('.hotrank_list').html(htmlStr)
+      }
+    }
+  })
+
+  // 5. 最新评论
+  $.ajax({
+    type:'get',
+    url:BigNew.latest_comment,
+    success:function(res){
+      console.log(res);
+      if(res.code==200){
+        var htmlStr = template('commentListTmp',res)
+        $('.comment_list').html(htmlStr)
       }
     }
   })
